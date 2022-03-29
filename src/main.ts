3f2301a5e29a0dbea9da7f9eda03f817831a417f
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 03:40:12 +0600

    added proxy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const API_SERVICE_URL = "https://message-send-app.herokuapp.com/data";
+  app.use('/json_placeholder', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/json_placeholder`]: '',
+    }
+  }));
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
