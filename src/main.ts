b996d0133559a2ce635a0da323775c67c75fbf60
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 04:00:09 +0600

    changed ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,16 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const API_SERVICE_URL = "https://message-send-app.herokuapp.com/";
-  app.use('/json_placeholder', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/json_placeholder`]: '',
-    }
-  }));
+  const API_SERVICE_URL = 'https://message-send-app.herokuapp.com/';
+
+  module.exports = function (app) {
+    app.use(
+      createProxyMiddleware({
+        target: API_SERVICE_URL,
+        changeOrigin: true,
+      }),
+    );
+  };
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
