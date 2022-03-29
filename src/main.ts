ee922337eaca3a520ab660530aab88f3e596379e
Git-Askhat <askat.shailoobekovich@mail.ru>
2022-03-30 05:07:14 +0600

    added bodyparser

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
 
   // const API_SERVICE_URL = 'https://message-send-app.herokuapp.com/';
 
